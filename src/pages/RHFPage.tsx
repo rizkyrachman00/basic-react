76444fd1a607ae7e6f683814c279f0a602134a32
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-02-21 16:34:18 +0700

    feat(form): add zod validation schema for form inputs

--- src/pages/RHFPage.tsx
+++ src/pages/RHFPage.tsx
@@ -1,19 +1,34 @@
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type RegisterFormSchema = {
-  username: string;
-  password: string;
-};
+// type RegisterFormSchema = {
+//   username: string;
+//   password: string;
+// };
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Minimal 3 karakter" })
+    .max(10, { message: "Maksimal 10 karakter" }),
+  password: z.string().min(8, { message: "Minimal 8 karakter" }),
+  age: z.coerce.number().min(18),
+  dob: z.coerce.date().optional(),
+});
+
+type RegisterFormSchema = z.infer<typeof registerFormSchema>;
 
 const RHFPage = () => {
-  
   // merupakan schema dari react hook form agar muncul suggestion
   // const form = useForm<{
   //   username: string;
   //   password: string;
-  // }>();  
+  // }>();
 
-  const form = useForm<RegisterFormSchema>();
+  const form = useForm<RegisterFormSchema>({
+    resolver: zodResolver(registerFormSchema),
+  });
 
   // const handleRegisterUser = (values: {
   //   username: string;
@@ -51,11 +66,33 @@ const RHFPage = () => {
           Username :
           <input type="text" {...form.register("username")} />
         </label>
+        <span style={{ color: "red" }}>
+          {form.formState.errors.username?.message}
+        </span>
 
         <label>
           Password :
           <input type="password" {...form.register("password")} />
         </label>
+        <span style={{ color: "red" }}>
+          {form.formState.errors.password?.message}
+        </span>
+
+        <label>
+          Age :
+          <input type="number" {...form.register("age")} />
+        </label>
+        <span style={{ color: "red" }}>
+          {form.formState.errors.age?.message}
+        </span>
+
+        <label>
+          Date of birth :
+          <input type="date" {...form.register("dob")} />
+        </label>
+        <span style={{ color: "red" }}>
+          {form.formState.errors.dob?.message}
+        </span>
 
         <button type="submit" style={{ width: "fit-content" }}>
           Register User
